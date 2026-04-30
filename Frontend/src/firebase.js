93054aced620c977ff43f67480b5c7406c21ab3e
Muhamad Rofiki <muhamadrofiki123@@gmail.com>
2026-04-30 11:24:16 +0700

    fix define db and auth

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // Tambahkan ini
+import { getAuth } from "firebase/auth";           // Tambahkan ini
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBq9lMekzm_aq-2Buvub7E7f7dx1V5kTiA",
   authDomain: "kasir-pintar-93e03.firebaseapp.com",
   projectId: "kasir-pintar-93e03",
-  storageBucket: "kasir-pintar-93e03.firebasestorage.app",
+  storageBucket: "kasir-pintar-93e03.appspot.com",
   messagingSenderId: "166529415845",
   appId: "1:166529415845:web:1a876202c5de0fb6b62b4c",
   measurementId: "G-P0PNQ3B54B"
@@ -19,3 +16,10 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Inisialisasi Database dan Auth (INI YANG TADI KURANG)
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+// Export agar bisa dipakai di App.jsx
+export { db, auth };
